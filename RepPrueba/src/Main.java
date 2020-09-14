6e61a4d3ba2826d9d3ed2d544665fa1c646437da
Isaac Barredo <isaacbarredo@gmail.com>
2020-09-14 13:27:19 +0200

    commit repo clonado

--- RepPrueba/src/Main.java
+++ RepPrueba/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Helloda");
+
+		int num1 = 1;
+		int num2 = 4;
+
+		System.out.println(num1 + num2)
 		
 		
 		
